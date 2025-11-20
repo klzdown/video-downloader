147227559e5fed24c00d6dcd931ee826cefda6cd
kalz <chnationg@gmail.com>
2025-11-20 18:43:40 +0700

    Fix API URL concatenation in fetchDownloadInfo

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function clearResult() {
 // =======================
 async function fetchDownloadInfo(videoUrl) {
   const apiUrl =
-    "https://www.tiktok.com/@umay_874/video/6996665911927262466" +
+    "https://www.tikwm.com/video/media/play/6990916139345808666.mp4" +
     encodeURIComponent(videoUrl) +
     "&hd=1";
 
